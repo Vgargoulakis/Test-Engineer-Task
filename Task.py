22df0d6aa263911921d749929895bd347caa1352
Evangelos Gargoulakis <vgargoulakis@gmail.com>
2020-03-19 20:05:57 +0000

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,40 @@
+import itertools
+ 
+ 
+def myTask( list ):               # Definition of the function
+ already_exists = ''
+ oneList = []
+ uniqueList = []
+ uniqueNumber = 0
+ totalNumber = 0
+ 
+# Print 1
+ for subList1 , subList2 in itertools.combinations( list , 2 ):
+   for element1 , element2 in itertools.product( subList1 , subList2 ):
+     if ( element1 == element2):
+       if ( already_exists != element1 ):
+         already_exists = element1
+         print ( "Strings that appear in more than one list:" , element1 )
+ 
+# Print 2
+ for subList in list :
+   for element in subList:
+     oneList.append(element)
+ 
+ for element in oneList :
+   if element not in uniqueList:
+     uniqueList.append(element)
+ 
+ for element in uniqueList:
+   uniqueNumber = uniqueNumber + 1
+ print ( "Number of unique strings:" , uniqueNumber )
+ 
+# Print 3
+ for element in oneList:
+   totalNumber = totalNumber + 1
+ print ( "Total number of strings processed:" , totalNumber )
+ 
+# List of character strings initialization
+listOfCharacterStr = [['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn']]
+ 
+myTask( listOfCharacterStr )        # Function call with a list as an argument
